4301e7255fe61e75f395743ab4406bbe7809a369
Shafique Dassu <shafd1231@gmail.com>
2023-09-29 01:42:28 +0100

    added to UIViewRepresentable: - remove double instances of "."

--- Budget/CurrencyFieldView.swift
+++ Budget/CurrencyFieldView.swift
@@ -23,21 +23,44 @@ struct CurrencyFieldView: UIViewRepresentable {
         textField.setContentHuggingPriority(.defaultHigh, for: .vertical)
         return textField
     }
-    //2. updates state of the UIView (when binding in use)
+    //2. updates state of the UIView (text field) (when binding in use)
     func updateUIView(_ uiView: UITextField, context: Context) {
-        
+        uiView.text = context.coordinator.textFor(value: amount)
     }
     //4. make aware of coordinator via function - create an instance of CFCoordinator class below because it is outside of struct. now context is aware of coordinator
     //context has a coordinator and gets access to this one here. 
     //make coordinator available through UIViewRep. Must edit return type to expected coordinator to be used.
     func makeCoordinator() -> CurrencyFieldCoordinator {
-        return CurrencyFieldCoordinator()
+        return CurrencyFieldCoordinator(amount: $amount)
     }
 }
 //3. coordinator used in order to make use of delegates on the view being brought in - without this function can't use delegates. make it conform to desired delegate (UITextFieldD) - gives us access to shouldChange delegate
 //returns a bool stating whether change is poss or not
 class CurrencyFieldCoordinator: NSObject, UITextFieldDelegate {
+    @Binding var amount: Double
+    
+    var formatter: NumberFormatter {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+        return formatter
+    }
+    
+    init(amount: Binding<Double>) {
+        _amount = amount
+    }
+    func textFor(value: Double) -> String? {
+        return formatter.string(from: value as NSNumber)
+    }
     func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
         return true
+    }
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        guard let currentText = textField.text else { return }
+        let strippedText = currentText.replacingOccurrences(of: ".", with: "").replacingOccurrences(of: "$", with: "")
+        guard let textDouble = Double(strippedText) else { return }
+        guard let formattedText = formatter.string(for: textDouble) else {
+            return
+        }
+        textField.text = formattedText
     }
 }
